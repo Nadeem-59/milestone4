786296933c70299e636770314e41c31fff96e6a8
Mustabshira khalid <160775326+Nadeem-59@users.noreply.github.com>
2024-09-14 01:23:40 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,63 @@
+document
+  .getElementById("resumeForm")
+  ?.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nameElement = document.getElementById("name") as HTMLInputElement;
+    const emailElement = document.getElementById("email") as HTMLInputElement;
+    const phoneElement = document.getElementById("phone") as HTMLInputElement;
+    const addressElement = document.getElementById(
+      "address"
+    ) as HTMLInputElement;
+
+    const educationElement = document.getElementById(
+      "education"
+    ) as HTMLInputElement;
+    const experienceElement = document.getElementById(
+      "experience"
+    ) as HTMLInputElement;
+    const skillsElement = document.getElementById("skills") as HTMLInputElement;
+
+    if (
+      nameElement &&
+      emailElement &&
+      phoneElement &&
+      addressElement &&
+      educationElement &&
+      experienceElement &&
+      skillsElement
+    ) {
+      const name = nameElement.value;
+      const email = emailElement.value;
+      const phone = phoneElement.value;
+      const address = addressElement.value;
+      const education = educationElement.value;
+      const experience = experienceElement.value;
+      const skills = skillsElement.value;
+
+      const resumeOutput = `
+        <h2>Resume</h2> 
+        <p><strong>Name:</strong> ${name} </p>
+        <p><strong>Email:</strong> ${email} </p>
+        <p><strong>Phone Number:</strong> ${phone} </p>
+        
+        <h3>Education</h3>
+        <p>${education}</p>
+
+        <h3>Experience</h3>
+        <p>${experience}</p>
+
+        <h3>Skills</h3>
+        <p>${skills}</p>    
+        `;
+
+      const resumeOutputElement = document.getElementById("resumeOutput");
+      if (resumeOutputElement) {
+        resumeOutputElement.innerHTML = resumeOutput;
+      } else {
+        console.error("Resume output element not found");
+      }
+    } else {
+      console.error("One Or More Form elements not found");
+    }
+  });
